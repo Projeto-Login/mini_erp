fd39a3a2f60aeec17c4c33cc769f90083a518d83
Nayara <naydoutor@gmail.com>
2025-07-21 14:37:46 -0300

    mundaças1

---
+++ app/database.py
@@ -0,0 +1,19 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+def conectar():
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Erro ao conectar ao banco de dados: {err}")
+        return None
